a1f3068ea85f07ab8d3c05b12b21a732f88ffbff
Shadow Chrome <marcelkoschau@yahoo.de>
2024-06-20 16:24:56 +0200

    improve feedback handling for saving character

--- frontend/src/components/wizard/Wizard.jsx
+++ frontend/src/components/wizard/Wizard.jsx
@@ -1,56 +1,62 @@
 import React, {useState} from 'react';
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
+import CharacterDetailsPage from './CharacterDetailsPage';
+import StatsPage from './StatsPage';
+import SkillsPage from './SkillsPage';
 import ProfessionBonusesPage from './ProfessionBonusesPage';
 import {rmuCharacterCreatorApi} from "../../requests/RmuCharacterCreatorApi";
 import {stats} from "../../common/Constants.js";
+import {Alert, Snackbar} from '@mui/material';
+
+const initialFormData = {
+    characterName: '',
+    playerName: '',
+    campaign: '',
+    sex: '',
+    age: '',
+    height: '',
+    weight: '',
+    build: '',
+    skin: '',
+    hair: '',
+    eyes: '',
+    race: '',
+    culture: '',
+    profession: '',
+    realm: '',
+    level: '1',
+    currentEP: '0',
+    epNextLevel: '10000',
+    agilityTemp: '',
+    agilityPot: '',
+    constitutionTemp: '',
+    constitutionPot: '',
+    empathyTemp: '',
+    empathyPot: '',
+    intuitionTemp: '',
+    intuitionPot: '',
+    memoryTemp: '',
+    memoryPot: '',
+    presenceTemp: '',
+    presencePot: '',
+    quicknessTemp: '',
+    quicknessPot: '',
+    reasoningTemp: '',
+    reasoningPot: '',
+    selfDisciplineTemp: '',
+    selfDisciplinePot: '',
+    strengthTemp: '',
+    strengthPot: '',
+    skills: {}, // Added field for skill learning data
+    bonuses: [], // Added field for profession bonuses
+    knacks: []  // Added field for knacks
+};
 
 const Wizard = () => {
     const [currentStep, setCurrentStep] = useState(1);
-    const [formData, setFormData] = useState({
-        characterName: '',
-        playerName: '',
-        campaign: '',
-        sex: '',
-        age: '',
-        height: '',
-        weight: '',
-        build: '',
-        skin: '',
-        hair: '',
-        eyes: '',
-        race: '',
-        culture: '',
-        profession: '',
-        realm: '',
-        level: '1',
-        currentEP: '0',
-        epNextLevel: '10000',
-        agilityTemp: '',
-        agilityPot: '',
-        constitutionTemp: '',
-        constitutionPot: '',
-        empathyTemp: '',
-        empathyPot: '',
-        intuitionTemp: '',
-        intuitionPot: '',
-        memoryTemp: '',
-        memoryPot: '',
-        presenceTemp: '',
-        presencePot: '',
-        quicknessTemp: '',
-        quicknessPot: '',
-        reasoningTemp: '',
-        reasoningPot: '',
-        selfDisciplineTemp: '',
-        selfDisciplinePot: '',
-        strengthTemp: '',
-        strengthPot: '',
-        skills: {}, // Added field for skill learning data
-        bonuses: [], // Added field for profession bonuses
-        descriptions: {} // Added field for skill descriptions
-    });
+    const [formData, setFormData] = useState(initialFormData);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [snackbarSeverity, setSnackbarSeverity] = useState('success');
 
     const nextStep = () => setCurrentStep((prev) => prev + 1);
     const prevStep = () => setCurrentStep((prev) => prev - 1);
@@ -94,7 +100,8 @@ const Wizard = () => {
     const handleSkillsChange = (skills) => {
         setFormData((prevFormData) => ({
             ...prevFormData,
-            skills
+            skills: skills.skills,
+            descriptions: skills.descriptions
         }));
     };
 
@@ -105,53 +112,79 @@ const Wizard = () => {
         }));
     };
 
-    const handleDescriptionChange = (descriptions) => {
+    const handleKnackChange = (knacks) => {
         setFormData((prevFormData) => ({
             ...prevFormData,
-            descriptions
+            knacks
         }));
     };
 
     const saveCharacter = async () => {
         try {
             await rmuCharacterCreatorApi.saveCharacter(formData);
-            alert('Character saved successfully!');
+            setSnackbarMessage('Character saved successfully!');
+            setSnackbarSeverity('success');
+            setSnackbarOpen(true);
+            resetForm();
         } catch (error) {
             console.error('Error saving character', error);
-            alert('Failed to save character.');
+            setSnackbarMessage('Failed to save character.');
+            setSnackbarSeverity('error');
+            setSnackbarOpen(true);
         }
     };
 
+    const resetForm = () => {
+        setFormData(initialFormData);
+        setCurrentStep(1);
+    };
+
     const handleNextStep = () => {
-        if (currentStep === 4) { // Adjust the step number according to your steps
+        if (currentStep === 4) {
             saveCharacter();
+        } else {
+            nextStep();
         }
-        nextStep();
+    };
+
+    const handleSnackbarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSnackbarOpen(false);
     };
 
     const stepContent = () => {
         switch (currentStep) {
             case 1:
-                return <Step1 formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
-                              setFormData={setFormData}/>;
+                return <CharacterDetailsPage formData={formData} handleInputChange={handleInputChange}
+                                             nextStep={handleNextStep}
+                                             setFormData={setFormData}/>;
             case 2:
-                return <Step2 formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
-                              prevStep={prevStep} rollStats={rollStats}/>;
+                return <StatsPage formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
+                                  prevStep={prevStep} rollStats={rollStats}/>;
             case 3:
                 return <ProfessionBonusesPage formData={formData} handleBonusChange={handleBonusChange}
-                                              nextStep={handleNextStep} prevStep={prevStep}/>;
+                                              handleKnackChange={handleKnackChange} nextStep={handleNextStep}
+                                              prevStep={prevStep}/>;
             case 4:
-                return <Step3 formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
-                              prevStep={prevStep} handleSkillsChange={handleSkillsChange}/>;
+                return <SkillsPage formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
+                                   prevStep={prevStep} handleSkillsChange={handleSkillsChange}/>;
             default:
-                return <Step1 formData={formData} handleInputChange={handleInputChange} nextStep={handleNextStep}
-                              setFormData={setFormData}/>;
+                return <CharacterDetailsPage formData={formData} handleInputChange={handleInputChange}
+                                             nextStep={handleNextStep}
+                                             setFormData={setFormData}/>;
         }
     };
 
     return (
         <div>
             {stepContent()}
+            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
+                <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{width: '100%'}}>
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
